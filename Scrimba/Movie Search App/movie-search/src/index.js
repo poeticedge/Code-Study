142d06933f038fc30b8f4733eacb14b687633c7a
Megan C. <soulfire2010@gmail.com>
2026-02-17 10:07:13 -0500

    React set up for movie app

---
+++ Scrimba/Movie Search App/movie-search/src/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div>
+          <h1>Hello World updated!</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById("root"));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
