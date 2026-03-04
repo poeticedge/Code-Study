7fd94eb8b9250a889eacf2e124126be9bda7586b
Megan C. <soulfire2010@gmail.com>
2026-03-04 14:03:02 -0500

    React Project Completed

--- Scrimba/Scrimba-react/src/App.jsx
+++ Scrimba/Scrimba-react/src/App.jsx
@@ -1,37 +1,55 @@
 import { createRoot } from "react-dom/client";
+import { Fragment } from "react";
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
 //import reactLogo from "./assets/react.svg";
 import "./App.css";
 
-function App() {
+function AppHeader() {
   return (
-    <>
+    <header className="header">
       <nav>
         <img
           src="https://res.cloudinary.com/control4/image/upload/v1771443981/06e12c91-7e08-4796-b2bc-b56e5d62972a/reactjs-icon.png"
-          className="logo"
+          className="AppLogo"
           alt="React logo"
-          width="40px"
         />
-        <h1>ReactFacts</h1>
+        <span>ReactFacts</span>
       </nav>
-      <main>
-        <h1>Fun facts about React</h1>
-        <ul>
-          <li>Was first released in 2013</li>
-          <li>Was originally created by Jordan Walke</li>
-          <li>Has well over 200K stars on GitHub</li>
-          <li>Is maintained by Meta</li>
-          <li>Powers thousands of enterprise apps, including mobile apps</li>
-          <li>test</li>
-        </ul>
-        <img
-          src="https://res.cloudinary.com/control4/image/upload/v1771444024/06e12c91-7e08-4796-b2bc-b56e5d62972a/reactjs-icon_2.png"
-          alt="React logo 2"
-        />
-      </main>
+    </header>
+  );
+}
+
+function MainContent() {
+  return (
+    <main className="mainContent">
+      <h1>Fun facts about React</h1>
+      <ul className="facts-list">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 200K stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>© 2026 Campbell development. All rights reserved.</small>
+    </footer>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <AppHeader />
+      <MainContent />
+      <Footer />
     </>
   );
 }
